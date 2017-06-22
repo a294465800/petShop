9d224ac3a67422da054d1ba5be00869953b9720d
SHINING <jack7770126@126.com>
2017-06-22 19:20:13 +0800

    [no]: just for save.

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,29 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    interval: 4000,
+    duration: 500,
+    userInfo: {},
+
+    //导航条
+    nav: [{
+      name: '商家动态',
+      id: 0
+    }, {
+      name: '用户评论',
+      id: 1
+    }, {
+      name: '商家信息',
+      id: 2
+    }]
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -16,10 +34,10 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
