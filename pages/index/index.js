4469c0e94b3f562e3bad0961f249057d98f0d35a
SHINING <jack7770126@126.com>
2017-06-24 11:44:10 +0800

    [new function]finally finish the index page. Include the phoneCall function and the map function.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,6 @@
 //index.js
 //获取应用实例
 let app = getApp()
-let timer_img = null
 //记录触摸位置
 let start = 0
 Page({
@@ -47,9 +46,9 @@ Page({
         avatar: '/images/head.jpg',
         img: [
           'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg'
+          'http://stu.hyes.tyc.edu.tw/blog/gallery/236/3807-1111.jpg',
+          'http://s2.buzzhand.net/uploads/d0/7/456525/14218526072156.jpg',
+          'http://imgapi.nownews.com/?w=640&q=60&src=http%3A%2F%2Fs.nownews.com%2Fa4%2F9e%2Fa49e049bc9aec44b9d7ee408d6450782.JPG'
         ]
       },
       {
@@ -59,9 +58,9 @@ Page({
         avatar: '/images/head.jpg',
         img: [
           'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498243041784&di=0da5739e9ef474746938dc78ec91629b&imgtype=0&src=http%3A%2F%2Fimg1.3lian.com%2Fimg013%2Fv4%2F18%2Fd%2F97.jpg'
+          'http://stu.hyes.tyc.edu.tw/blog/gallery/236/3807-1111.jpg',
+          'http://s2.buzzhand.net/uploads/d0/7/456525/14218526072156.jpg',
+          'http://imgapi.nownews.com/?w=640&q=60&src=http%3A%2F%2Fs.nownews.com%2Fa4%2F9e%2Fa49e049bc9aec44b9d7ee408d6450782.JPG'
         ]
       }
     ],
@@ -87,7 +86,24 @@ Page({
         content: '瞬间爆炸',
         commentNumber: 22
       }
-    ]
+    ],
+
+    //店家信息
+    store: {
+      id: 0,
+      name: '瑞文宠物寄养中心',
+      address: '广州市番禺区市桥街道',
+      runtime: '9:00 - 20:00 全年无休',
+      tel: '123-4567-8910',
+      description: '我們是專業的犬舍，跟普通的寵物店大有不同。我們接受無數電視台和雜誌訪問，在犬展場上獲勝無數，所出售的優秀幼犬，經獸醫檢驗，不帶有遺傳問題。有14天健康合約保障。',
+      imgs: [
+        'http://www.okaydj.com/uploads/allimg/140313/1-140313130R4.jpg',
+        'https://img.grouponcdn.com/deal/js6sX3YFGJHf3rW8fT8E/KC-700x400',
+        'http://www.teepr.com/wp-content/uploads/2016/03/11040961_453333991524522_484123248141614718_o.jpg',
+        'http://img1.100ye.com/img2/4/34/239/9051239/msgpic/35185982.jpg',
+        'http://pet.zoneonezone.com/files/shop/img/1068/Pretty_Pet_Shop_Gallery_6.jpg'
+      ]
+    }
   },
   onLoad() {
     console.log('onLoad')
@@ -108,7 +124,6 @@ Page({
 
   //根据动作判断隐藏轮播
   touchEnd(e) {
-    clearTimeout(timer_img)
     const that = this
     let animation = wx.createAnimation({
       duration: 500,
@@ -120,11 +135,6 @@ Page({
       that.setData({
         animationImg: animation.export()
       })
-      // timer_img = setTimeout(() => {
-      //   that.setData({
-      //     'flag.nav_flag': true,
-      //   })
-      // }, 200)
     }
   },
 
@@ -138,7 +148,6 @@ Page({
     animation = animation.height('300rpx').step()
     that.setData({
       animationImg: animation.export(),
-      // 'flag.nav_flag': false,
     })
   },
 
@@ -171,10 +180,45 @@ Page({
   },
 
   //图片预览
-  preImg(e){
+  preImg(e) {
     wx.previewImage({
-      current: e.currentTarget.dataset.id,
+      current: e.currentTarget.dataset.url,
       urls: e.currentTarget.dataset.imggup,
+    })
+  },
+
+  //打开商家地图
+  openLocation() {
+    wx.getLocation({
+      type: 'gcj02',
+      success: res => {
+        let latitude = res.latitude
+        let longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      },
+    })
+  },
+
+  //电话商家
+  callStore() {
+    console.log(1)
+    const that = this
+    wx.makePhoneCall({
+      phoneNumber: that.data.store.tel,
+    })
+  },
+
+  //商家图片预览
+  preStoreImg(e) {
+    const that = this
+    console.log(e)
+    wx.previewImage({
+      current: e.currentTarget.dataset.url,
+      urls: that.data.store.imgs,
     })
   }
 })
