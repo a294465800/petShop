69acda5fb924f087b5f7b93eb016805802814a5d
SHINING <jack7770126@126.com>
2017-06-30 17:40:52 +0800

    [new function] now commodity comment can add some pics.

--- pages/cost_comment/cost_comment.js
+++ pages/cost_comment/cost_comment.js
@@ -8,6 +8,9 @@ Page({
     //评论总星数
     star_count: [],
 
+    //上传图片
+    imgs: [],
+
     //星星图片
     star_img: {
       ok: '/images/shop/star_f.png',
@@ -33,8 +36,48 @@ Page({
     })
   },
 
+  //图片上传
+  addImg() {
+    const that = this
+    wx.chooseImage({
+      count: 3,
+      sizeType: ['compressed'],
+      success: res => {
+        let temp = [...that.data.imgs, ...res.tempFilePaths]
+        if(temp.length > 3){
+          temp.length = 3
+        }
+        that.setData({
+          imgs: temp
+        })
+      },
+    })
+  },
+
+  //图片预览
+  preImg(e) {
+    let img = e.currentTarget.dataset.img
+    let imgs = e.currentTarget.dataset.imgs
+    wx.previewImage({
+      current: img,
+      urls: imgs
+    })
+  },
+
+  //删除图片
+  delImg(e) {
+    const that = this
+    let img = e.currentTarget.dataset.img
+    let imgs = that.data.imgs
+    let index = imgs.indexOf(img)
+    imgs.splice(index, 1)
+    that.setData({
+      imgs: imgs
+    })
+  },
+
   //店铺评分
-  getScore(e){
+  getScore(e) {
     const that = this
     let id = e.currentTarget.dataset.index
     let type = e.currentTarget.dataset.type
