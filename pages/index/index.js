6e2833f28880610d117073891222d180046faf29
SHINING <jack7770126@126.com>
2017-07-13 08:41:55 +0800

    [new function] add the ad image.

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,8 @@ Page({
   data: {
     shop: app.globalData.shop,
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
+      '/images/ad.jpg'
+      ],
     interval: 4000,
     duration: 500,
     userInfo: {},
